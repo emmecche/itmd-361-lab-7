f5f8497b91c546d4d80c5825bdb36356054be2e2
pfisler <pfisler@hawk.iit.edu>
2021-04-12 22:10:16 -0500

    added javascript functionality described by the lab handout

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function init(){
     //add your javascrip between these two lines of code
-     
-    
-    
-    
-    
-    
-    
-    
-    window.addEventListener('load', init);
+
+    var info    = document.getElementsByClassName("center");
+    const name  = info[1].textContent;
+    //const email = info[2].textContent;
+
+    var button  = document.getElementById("entrybutton");
+    var input   = document.getElementById("entryinput");
+    var output  = document.getElementById("textoutput");
+
+    function buttonPress() {
+        if (input.value == '') {
+            alert("Empty Input");
+        } else {
+            alert(name + ": " + input.value);
+            output.textContent = input.value;
+        }
+    }
+    button.addEventListener('click', buttonPress);
+
 }
+    
+window.addEventListener('load', init);
